cb13927afd333468cb538323917c580db96dc115
Nirajchemjong <nirajlimbu7@gmail.com>
2023-06-20 21:13:09 +1000

    bug fixed

--- js/contact_us.js
+++ js/contact_us.js
@@ -1,26 +1,30 @@
 //contact us form
 $(".contact_btn").on("click", function () {
-  event.preventDefault();
+  // event.preventDefault();
   var name = document.getElementById("name").value;
   var email = document.getElementById("email").value;
   var phone = document.getElementById("phone").value;
   var message = document.getElementById("message").value;
 
-  var mailtoLink =
-    "mailto:nirajlimbu7@gmail.com" +
-    "?subject=" +
-    encodeURIComponent("New Message from Contact Form") +
-    "&body=" +
-    encodeURIComponent(
-      "Name: " +
-        name +
-        "\nEmail: " +
-        email +
-        "\nMessage: " +
-        message +
-        "\nPhone: " +
-        phone
-    );
+  if (name !== "" && email !== "" && phone !== "" && message !== "") {
+    var mailtoLink =
+      "mailto:nirajlimbu7@gmail.com" +
+      "?subject=" +
+      encodeURIComponent("New Message from Contact Form") +
+      "&body=" +
+      encodeURIComponent(
+        "Name: " +
+          name +
+          "\nEmail: " +
+          email +
+          "\nMessage: " +
+          message +
+          "\nPhone: " +
+          phone
+      );
 
-  window.location.href = mailtoLink;
+    window.location.href = mailtoLink;
+  } else {
+    alert("Please check your information!");
+  }
 });
